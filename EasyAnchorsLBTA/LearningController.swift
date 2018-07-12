567a7dc0516b2283e8bd3af9cdd221cdeb2d86e2
carlogilmar <carlogilmar12@gmail.com>
2018-07-11 21:12:38 -0500

    Adding subviews

--- EasyAnchorsLBTA/LearningController.swift
+++ EasyAnchorsLBTA/LearningController.swift
@@ -6,12 +6,40 @@
 //  Copyright © 2018 Carlo Gilmar. All rights reserved.
 //
 import UIKit
+import PureLayout
 
-class LearningController: UIViewController {
+class LearningController: UIView{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .yellow
+    let bannerView: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.backgroundColor = .gray
+        view.autoSetDimension(.height, toSize: UIScreen.main.bounds.size.width / 3)
+        return view
+    }()
+    
+    let profileView: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.backgroundColor = .gray
+        view.layer.borderColor = UIColor.white.cgColor
+        view.layer.borderWidth = 1.0
+        view.layer.cornerRadius = 5.0
+        return view
+    }()
+    
+    let segmentedControl: UISegmentedControl = {
+        let sc = UISegmentedControl(items: ["Tweets", "Media", "Likes"])
+        return sc
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(bannerView)
+        self.addSubview(profileView)
+        self.addSubview(segmentedControl)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
 }
