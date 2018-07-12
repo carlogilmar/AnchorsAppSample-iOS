b90d59a698b855cac725e7a66d59ecfb8fc56113
carlogilmar <carlogilmar12@gmail.com>
2018-07-11 20:47:49 -0500

    Creating a new ViewController

---
+++ EasyAnchorsLBTA/LearningController.swift
@@ -0,0 +1,17 @@
+//
+//  LearningController.swift
+//  EasyAnchorsLBTA
+//
+//  Created by Carlo Gilmar on 11/07/18.
+//  Copyright © 2018 Carlo Gilmar. All rights reserved.
+//
+import UIKit
+
+class LearningController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+}
